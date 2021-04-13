d3c0e738743f87814ad6b53c6992cfc674af066e
esgn <emmanuel.seguin@ign.fr>
2021-04-13 20:26:02 +0200

    corrections checkhn + fix readme

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # add-ign-id-to-ban-ign-csv
 
-* `get_csv.py` pour récupérer les données
-* `check_hn.py` pour vérifier le contenu des csv housenumber-id-ign
-* `merge_csv.py` pour rajouter l'id ign au csv de la ban ign
-* `check_result.py` pour vérifier les fichiers résultats obtenus
+## Récupération des fichiers de la BAN IGN 
 
+Le script `get_csv.py` récupère les CSV `ban-dept` et `housenumber-id-ign-dept`.
+
+Les fichiers `housenumber-id-ign-dept` effectuent le lien entre une `id_ban_adresse` IGN et une `cleabs` IGN c'est à dire l'identifiant unique de l'adresse dans la BDTOPOV3 de l'IGN.
+
+Le script `check_hn` permet de s'assurer du contenu des fichiers `housenumber-id-ign-dept`.
+* Vérification de l'absence de doublons sur `id_ban_adresse` et `cleabs` 
+* Vérification de l'absence de valeur nulle
+
+
+## Ajout de la `cleabs` IGN aux fichiers `ban-dept`
+
+Le script `merge_csv.py` permet de rajouter aux fichiers `ban-dept` une colonne `ign` contenant la `cleabs` IGN correspondant à l'`id_ban_adresse`. Les fichiers résultats sont produits dans le dossier `ban-ign-id`.
+
+Le script `check_result.py` permet de vérifier succinctement le contenu des fichiers résultats obtenus.
--- check_hn.py
+++ check_hn.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import pandas as pd 
+import pandas as pd
 import os
 import sys
 import re
@@ -13,10 +13,20 @@ hn_p = re.compile("^housenumber-id-ign-([AB0-9]+).csv.gz")
 for hn_file in os.listdir(hn_dir):
     if hn_p.match(hn_file):
         print("Vérification de " + str(hn_file))
-        df_hn = pd.read_csv(os.path.join(hn_dir,hn_file),sep=';',encoding='utf-8',dtype=object)
+        df_hn = pd.read_csv(os.path.join(hn_dir, hn_file),
+                            sep=';', encoding='utf-8', dtype=object)
         if df_hn['id_ban_adresse'].duplicated().any():
-            "Doublons sur id_ban_adresse"
+            print("[ERREUR] Doublons sur id_ban_adresse")
+            sys.exit(0)
         if df_hn['ign'].duplicated().any():
-            "Doublons sur ign"
-        if len(df_hn[df_hn.duplicated()].index):
-            "Doublons sur le couple id_ban_adresse, ign"
+            print("[ERREUR] Doublons sur ign")
+            sys.exit(0)
+        if df_hn['id_ban_adresse'].isnull().values.any():
+            print("[ERREUR] Valeurs nulles dans id_ban_adresse")
+            sys.exit(0)
+        if df_hn['ign'].isnull().values.any():
+            print("[ERREUR] Valeurs nulles dans id_ban_adresse")
+            sys.exit(0)
+    else:
+        print("[ERREUR] Nom du fichier invalide : " + hn_file)
+        sys.exit(0)
