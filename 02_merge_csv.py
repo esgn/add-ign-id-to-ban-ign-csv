b90fadd0ba59a507cb613dc214d70a0eaa317c66
Emmanuel S <5435148+esgn@users.noreply.github.com>
2021-11-09 16:35:05 +0100

    Parallel version

--- 02_merge_csv.py
+++ 02_merge_csv.py
@@ -6,31 +6,32 @@ import os
 import sys
 import re
 import shutil
+from multiprocessing.pool import ThreadPool
 
 ban_p = re.compile("^ban-([AB0-9]+).csv.gz")
 hn_t = "housenumber-id-ign-{}.csv.gz"
 out_dir = "ban-ign-id"
+jobs = 6
 
 if os.path.exists(out_dir):
     shutil.rmtree(out_dir)
 os.mkdir(out_dir)
 
-for ban_f in os.listdir("ban"):
-    if ban_p.match(ban_f):
-        print("Traitement de " + str(ban_f))
-        n = re.search(ban_p,ban_f).group(1)
-        hn_f = hn_t.format(n)
-        df_ban = pd.read_csv("ban/"+ban_f,sep=';',encoding='utf-8',dtype=object)
-        df_hn = pd.read_csv("housenumber-id-ign/"+hn_f,sep=';',encoding='utf-8')
-        result = df_ban.merge(df_hn,how='left',on='id_ban_adresse')
-        # Vérifications rapides
-        if result['ign'].isnull().values.any():
-            print("Valeur manquante dans la colonne ign")
-            print(str(len(result[result['ign'].isna()].index)) + " valeurs nulles")
-        if len(df_ban.index) != len(result.index):
-            print("Le fichier source et résultat ne sont pas de la même taille")
-            print("source : " + str(len(df_ban.index)) + " lignes")
-            print("source : " + str(len(result.index)) + " lignes")
-            sys.exit(0)
-        out_file = 'ban-'+n+'.csv.gz'
-        result.to_csv(os.path.join(out_dir,out_file),index=False,encoding='utf-8',sep=';',compression='gzip')
+def merge_file(ban_f):
+    n = re.search(ban_p,ban_f).group(1)
+    hn_f = hn_t.format(n)
+    df_ban = pd.read_csv("ban/"+ban_f,sep=';',encoding='utf-8',dtype=object)
+    df_hn = pd.read_csv("housenumber-id-ign/"+hn_f,sep=';',encoding='utf-8')
+    result = df_ban.merge(df_hn,how='left',on='id_ban_adresse')
+    out_file = 'ban-'+n+'.csv.gz'
+    result.to_csv(os.path.join(out_dir,out_file),index=False,encoding='utf-8',sep=';',compression='gzip')
+    return str(ban_f) + " traité"
+
+def main():
+    file_list = os.listdir("ban")
+    results = ThreadPool(jobs).imap_unordered(merge_file,file_list)
+    for r in results:
+        print(r)
+
+if __name__ == "__main__":
+    main()
